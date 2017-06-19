a8748f31049113dce56562a7a8b85a2732d924a5
Andrew Sharp <andrew.sharp57@gmail.com>
2017-06-19 18:14:29 -0500

    use full syntax

--- src/cljs/modern_cljs/core.cljs
+++ src/cljs/modern_cljs/core.cljs
@@ -10,7 +10,7 @@
 (rum/defc header [] [:h1 "Tasks"])
 
 (rum/defc form []
-  [:form {:on-submit #(false)}
+  [:form {:on-submit (fn [_] false)}
     [:input {:type "text"}]
     [:input {:type "submit"}]])
 
