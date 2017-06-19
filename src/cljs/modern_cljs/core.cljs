c64ece5498fe6f8d423b90f0aabd2a06d85b7aac
Andrew Sharp <andrew.sharp57@gmail.com>
2017-06-19 15:17:51 -0500

    list static tasks

--- src/cljs/modern_cljs/core.cljs
+++ src/cljs/modern_cljs/core.cljs
@@ -4,6 +4,15 @@
 
 (enable-console-print!)
 
-(rum/defc app [] [:div.app "Hello!"])
+(def task-list [{:description "buy milk"} {:description "groom dog"}])
+
+(rum/defc header [] [:h1 "Tasks"])
+
+(rum/defc task [t] [:li (:description t)])
+
+(rum/defc tasks []
+  [:ol (map-indexed #(rum/with-key (task %2) %1) task-list)])
+
+(rum/defc app [] [:div.app (header) (tasks)])
 
 (rum/mount (app) (js/document.getElementById "app"))
